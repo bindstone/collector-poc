0b03dbd93ac4018281c419932259ddccd3274cda
Mike Schilling <msbindstone@gmail.com>
2018-01-22 21:11:21 +0100

    Init

--- src/main/java/com/bindstone/collector/CollectorApplication.java
+++ src/main/java/com/bindstone/collector/CollectorApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CollectorApplication {
-    //https://jira.atlassian.com/rest/api/2/mypermissions
-    //https://www.programcreek.com/java-api-examples/index.php?source_dir=arquillian-governor-master/jira/
-
     public static void main(String[] args) {
         SpringApplication.run(CollectorApplication.class, args);
     }
